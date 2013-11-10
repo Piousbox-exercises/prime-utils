9e60921deac8dc2e10c785a682cb3fee190f5890
Victor Piousbox <piousbox@gmail.com>
2013-11-09 19:20:51 -0800

    #n_primes works

--- lib/prime_utils.rb
+++ lib/prime_utils.rb
@@ -1,7 +1,49 @@
 
 class PrimeUtils
 
+  def ulimit= n
+    @ulimit = n
+  end
+
+  def initialize
+    @memoized = {} # stores true if composite
+    @ulimit = 150
+    @primes = []
+  end
+
   def self.n_primes n
+    item = self.new
+    return item.n_primes n
+  end
+
+  def n_primes n
+    if @primes.length < n
+      initialize()
+      @ulimit = @ulimit * 2
+      compute
+      return n_primes n
+    else
+      return @primes.take( n )
+    end
+  end
+
+  def compute current = 2
+    # puts! current
+    # puts! @memoized
+    return if current > @ulimit
+    
+    if @memoized[current]
+      # current is composite, switch to next
+      return compute( current + 1 )
+    else
+      @primes << current
+      ttt = current + current # ttt is steps of length current, up to @ulimit
+      while ttt < @ulimit do
+        @memoized[ ttt ] = true
+        ttt = ttt + current
+      end
+      return compute( current + 1 )
+    end
   end
 
   def self.prime? n
--- spec/prime_utils_spec.rb
+++ spec/prime_utils_spec.rb
@@ -16,4 +16,39 @@ describe PrimeUtils do
     end
   end
 
+  # describe '#succ' do
+  #   it 'is defined' do
+  #      PrimeUtils.methods.include?( :succ ).should eql true
+  #   end
+  # end
+
+  describe '#n_primes' do
+    before :each do
+      @prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+    end
+
+    it 'is defined' do
+      PrimeUtils.methods.include?( :n_primes ).should eql true
+    end
+
+    it 'computes with small ulimit' do
+      utils = PrimeUtils.new
+      utils.ulimit = 10
+      answer = utils.n_primes( @prime_list.length )
+      answer.should eql @prime_list
+    end
+
+    it 'test against an existing list of primes' do
+      answer = PrimeUtils.n_primes( @prime_list.length )
+      answer.should eql @prime_list
+    end
+    
+  end
+
+  describe '#compute' do
+    it 'is defined' do
+      PrimeUtils.instance_methods.include?( :compute ).should eql true
+    end
+  end
+
 end
